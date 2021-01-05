a60266e9fab5e99df23ad34d98ff28ad14c4b835
Diana Carolina Quintero <karodev3@gmail.com>
2021-01-05 14:10:35 -0500

    0x03. Minimum Operations

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 0
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 1
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 3
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 4
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 5
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 6
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 7
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 8
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 9
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 10
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 11
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 12
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 21
+print("n = {} -> {}".format(n, minOperations(n)))
+
+n = 30
+print("n = {} -> {}".format(n, minOperations(n)))
