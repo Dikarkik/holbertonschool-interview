39cd73fc3a1e192ec7234d4edb51cc5b43b773d9
Diana Carolina Quintero <karodev3@gmail.com>
2021-01-06 00:02:31 -0500

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
-	len = 1 # "H"
-	operations = 0
-	copy = 0
+    len = 1  # "H"
+    operations = 0
+    copy = 0
 
-	while (len <= n):
+    while (len <= n):
 
-		if (len == n):
-			return operations
+        if (len == n):
+            return operations
 
-		# "Copy All"
-		if (n % len == 0):
-			copy = len
-			operations += 1
+        # "Copy All"
+        if (n % len == 0):
+            copy = len
+            operations += 1
 
-		# "Paste"
-		len = len + copy
-		operations += 1
+        # "Paste"
+        len = len + copy
+        operations += 1
 
-	return 0
+    return 0
