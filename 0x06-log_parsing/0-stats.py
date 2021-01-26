b0b8492a0eeb6440ae0d49043fa7048c4ad1bdc0
Diana Carolina Quintero <karodev3@gmail.com>
2021-01-26 15:12:22 -0500

    0x06. Log Parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# Script that reads stdin line by line and computes metrics.
+# After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+# statistics from the beginning: Total file size and number of lines by status code.
+# excecute: ./0-generator.py | ./0-stats.py
+import sys
+
+
+count = 1
+total_size = 0
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+
+
+def print_metrics():
+
+    print(f'File size: {total_size}')
+
+    for key, value in status_code.items():
+
+        if value > 0:
+            print(f'{key}: {value}')
+
+
+try:
+
+    for line in sys.stdin:
+
+        total_size += int(line.split()[8])
+
+        status_code[line.split()[7]] += 1
+
+        # print metrics every 10 lines
+        if (count % 10 == 0):
+            print_metrics()
+
+        count += 1
+
+except KeyboardInterrupt:
+
+    print_metrics()
+    sys.exit(0)
