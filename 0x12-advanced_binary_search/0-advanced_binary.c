f350438459875214c5b401855a70e4dc0f3190ea
Diana Carolina Quintero <karodev3@gmail.com>
2021-04-28 11:42:50 -0500

    Add 0x12. Advanced Binary Search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,66 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+int binary_search(int *array, int value, int left, int right);
+void print_subarray(int *array, int left, int right);
+int binary_search_recursive(int *array, int value, size_t start, size_t end);
+/**
+ * advanced_binary - searches for a value in a sorted array of integers.
+ * You can assume that array will be sorted in ascending order.
+ *
+ * @array: is a pointer to the first element of the array to search in.
+ * @size: is the number of elements in array.
+ * @value: is the value to search for.
+ *
+ * Return: return the index of the first value in the array.
+ * If value is not present in array or if array is NULL,
+ * your function must return -1.
+ *
+ * gcc -Wall -Wextra -Werror -pedantic 0-main.c 0-advanced_binary.c -o 0-advanced_binary
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array && size > 0)
+		return (binary_search(array, value, 0, size - 1));
+
+	return (-1);
+}
+
+int binary_search(int *array, int value, int left, int right)
+{
+	int middle = (right + left) / 2;
+
+	print_subarray(array, left, right);
+
+	if (left == right && array[middle] != value)
+		return (-1);
+
+	if (array[middle] == value)
+	{
+		if (middle > 0 && array[middle - 1] == value)
+			return (binary_search(array, value, left, middle));
+		else
+			return (middle);
+	}
+	else if (value < array[middle])
+		return (binary_search(array, value, left, middle - 1));
+	else
+		return (binary_search(array, value, middle + 1, right));
+}
+
+void print_subarray(int *array, int left, int right)
+{
+	int i;
+
+	printf("Searching in array: ");
+
+	for (i = left; i <= right; i++)
+	{
+		printf("%i", array[i]);
+
+		if (i < right)
+			printf(", ");
+	}
+
+	printf("\n");
+}
